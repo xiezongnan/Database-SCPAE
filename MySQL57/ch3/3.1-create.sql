c1114e8cf3fecea9c0437b0bc8ffc22133fb6873
Petrus <xiezongnan@foxmail.com>
2018-12-19 16:22:05 +0800

    first commit

---
+++ MySQL57/ch3/3.1-create.sql
@@ -0,0 +1,12 @@
+-- 查看当前所有存在的数据库
+show databases;
+
+-- 创建数据库
+-- create database database_name;
+create database test_db;
+
+-- 查看数据库定义
+show create database test_db;
+
+
+
---
+++ MySQL57/ch3/3.2-drop.sql
@@ -0,0 +1,7 @@
+-- 删除数据库
+drop database test_db;
+
+-- 删除后使用 show create database test_db
+-- 15:43:09	show create database test_db	Error Code: 1049. Unknown database 'test_db'	0.000 sec
+
+
